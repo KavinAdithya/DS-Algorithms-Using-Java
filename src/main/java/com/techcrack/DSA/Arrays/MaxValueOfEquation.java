fc11715c6a0f79875128031ffc8a3a9f0bd92f33
KaVin S <kavinadithya3@gmail.com>
2024-05-14 20:47:06 +0530

    Solved 4 problems of leetcode conecpt binary search

--- src/main/java/com/techcrack/DSA/Arrays/MaxValueOfEquation.java
+++ src/main/java/com/techcrack/DSA/Arrays/MaxValueOfEquation.java
@@ -11,19 +11,30 @@ public class MaxValueOfEquation {
         PriorityQueue<int[]> pq=new PriorityQueue<>((prev,current)->current[1]-prev[1]);
 
         for(int[] point:points){
-            pq.offer(point);
+
             for(int[]  p:pq)
                 System.out.println(Arrays.toString(p));
 
-            System.out.println("Max "+ Arrays.toString(pq.peek()));
+            while(!pq.isEmpty()&&point[0]-pq.peek()[0]>k)
+                pq.poll();
+
+            if(!pq.isEmpty()){
+                int[] localValue=pq.peek();
+                int localMax=point[1]-localValue[0]+localValue[1]+point[0];
+                max=Math.max(localMax,max);
+            }
+
+            pq.offer(new int[]{point[1]-point[0],point[0]});
         }
         return max;
     }
 
     public static void main(String[] args) {
-        MaxValueOfEquation maxValueEquation=new MaxValueOfEquation();
-        int[][] points={{1,3},{3,0},{9,4},{10,2}};
+        MaxValueOfEquation maxValueEquation = new MaxValueOfEquation();
+        int[][] points = {{1,3},{2,0},{5,10},{6,-10}};
 
-        maxValueEquation.findMaxValueOfEquation(points,9);
+        int k=maxValueEquation.findMaxValueOfEquation(points, 1);
+
+        System.out.println(k);
     }
 }
---
+++ src/main/java/com/techcrack/DSA/Arrays/MissingPositiveNumber.java
@@ -0,0 +1,21 @@
+package com.techcrack.DSA.Arrays;
+
+public class MissingPositiveNumber {
+    public int findKthPositive(int[] nums, int k) {
+        int start=0;
+        int end=nums.length-1;
+
+
+        while(start<=end){
+            int mid=(start+end)/2;
+            int missingCount=(nums[mid]-(mid+1));
+            if(missingCount<k)
+                start=mid+1;
+            else
+                end=mid-1;
+        }
+        if(end==-1)
+            return k;
+        return nums[end]+ (k-(nums[end]-(end+1)));
+    }
+}
