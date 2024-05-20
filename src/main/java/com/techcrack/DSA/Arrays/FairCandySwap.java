566dfa83cda3ba38e1ded82423599bf359d0df28
KaVin S <kavinadithya3@gmail.com>
2024-05-20 11:20:13 +0530

    Search2DArray.java and MinimumAbsoluteSumDifference.java problems are solved

--- src/main/java/com/techcrack/DSA/Arrays/FairCandySwap.java
+++ src/main/java/com/techcrack/DSA/Arrays/FairCandySwap.java
@@ -1,5 +1,6 @@
 package com.techcrack.DSA.Arrays;
 
+import java.util.Arrays;
 public class FairCandySwap {
     public int[] fairCandySwap(int[] aliceSizes, int[] bobSizes) {
 
@@ -21,5 +22,47 @@ public class FairCandySwap {
                 return new int[]{aliceSizes[k],searchElement};
         }
         return null;
+    }
+}
+
+class Solution {
+    public int[] fairCandySwap(int[] aliceSizes, int[] bobSizes) {
+
+        int candiesAlice = countCandies(aliceSizes);
+        int candiesBob = countCandies(bobSizes);
+        int sameCandies = (candiesAlice - candiesBob) / 2;
+
+        Arrays.sort(aliceSizes);
+
+        for(int bobCandy : bobSizes){
+            int aliceCandy = binarySearch(aliceSizes, bobCandy + sameCandies);
+            if(aliceCandy != -1)
+                return new int[]{aliceCandy,bobCandy};
+        }
+        return new int[]{-1,-1};
+    }
+
+    private int countCandies(int[] candies){
+        int candie = 0;
+        for(int candy : candies)
+            candie += candy;
+        return candie;
+    }
+
+    private int binarySearch(int[] candies, int targetCandy){
+        int start = 0;
+        int end = candies.length - 1;
+
+        while(start <= end){
+            int mid = (start + end) / 2;
+            if(candies[mid] == targetCandy)
+                return targetCandy;
+            else if(candies[mid] > targetCandy)
+                end = mid - 1;
+            else
+                start = mid + 1;
+        }
+
+        return -1;
     }
 }
