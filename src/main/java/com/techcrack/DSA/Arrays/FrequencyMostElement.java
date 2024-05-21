3588f8a96f60607384ca4f116fbbe968db20bca8
KaVin S <kavinadithya3@gmail.com>
2024-05-21 18:04:51 +0530

    FrequencyMostElement.java and PeakElementII.java problems has been solved

---
+++ src/main/java/com/techcrack/DSA/Arrays/FrequencyMostElement.java
@@ -0,0 +1,55 @@
+package com.techcrack.DSA.Arrays;
+
+import java.util.Arrays;
+
+public class FrequencyMostElement {
+
+    public int maxFrequency(int[] num, int operation) {
+
+        int frequencyCount = 1;
+        long[] numSum = new long[num.length];
+
+        Arrays.sort(num);
+
+        numSum[0] = num[0];
+
+        for(int k = 1; k < num.length; k++)
+            numSum[k] =  numSum[k-1] + num[k];
+
+        System.out.println(numSum[numSum.length - 1]);
+        for(int k = num.length - 1; k > 0; k--){
+            int count = findFrequency(num, numSum, k, operation);
+            if(count > frequencyCount)
+                frequencyCount = count;
+        }
+
+        return frequencyCount;
+
+    }
+
+    private int findFrequency(int[] num, long[] numSum, int targetIndex, long operation){
+        int target = num[targetIndex];
+        int start = 0;
+        int end = targetIndex - 1;
+        int count = 1;
+
+        while(start <= end){
+
+            int mid = (start + end) / 2;
+            long value = numSum[end] - numSum[mid] + num[mid] + operation;
+            long expected = (long)(end - mid + 1) * num[targetIndex];
+            if(value == expected)
+                return count + (end - mid + 1);
+            else if(value > expected){
+                operation = value - expected;
+                count += (end - mid + 1);
+                end = mid - 1;
+            }
+            else
+                start = mid + 1;
+        }
+
+        return count;
+    }
+
+}
