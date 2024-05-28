23550e46cd4cf50ebb46ca406a88f001197a0e9d
KaVin S <kavinadithya3@gmail.com>
2024-05-28 10:16:59 +0530

    MountainPeak And SplitArray.java problems solved

---
+++ src/main/java/com/techcrack/DSA/Arrays/MountainArray.java
@@ -0,0 +1,10 @@
+package com.techcrack.DSA.Arrays;
+
+public class MountainArray {
+    public int length(){
+        return 5;
+    }
+    public int get(int index){
+        return index + 6;
+    }
+}
