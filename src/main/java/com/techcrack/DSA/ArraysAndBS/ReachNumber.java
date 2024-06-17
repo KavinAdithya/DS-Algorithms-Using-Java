de22c031a5e877a161573583300fd73a56f97070
KaVin S <kavinadithya3@gmail.com>
2024-06-17 06:13:38 +0530

    Revisited Reach number problem

--- src/main/java/com/techcrack/DSA/ArraysAndBS/ReachNumber.java
+++ src/main/java/com/techcrack/DSA/ArraysAndBS/ReachNumber.java
@@ -33,4 +33,39 @@ public class ReachNumber {
             return (int)steps + 2;
         }
     }
+
+    //Revisited Answer
+    public int reachNumber1(int target) {
+        long target1 = Math.abs(target);
+
+        long start = 1;
+        long end = target1 ;
+
+
+        while(start <= end){
+            long mid = (start + end) / 2;
+
+            long midSum = mid * (mid + 1) / 2;
+
+            if(midSum == target1)
+                return (int)mid;
+            else if(midSum > target1)
+                end = mid - 1;
+            else
+                start = mid + 1;
+
+        }
+
+        long startSum = start * (start + 1) / 2;
+
+        if((startSum - target1) % 2 == 0 )
+            return (int)start;
+        else {
+            if(end % 2 == 0)
+                return (int)start + 2;
+            else
+                return (int)start + 1;
+        }
+
+    }
 }
