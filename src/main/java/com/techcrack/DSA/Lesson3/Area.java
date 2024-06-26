5b13c31d571a100d19f096f8a708129444e8c2aa
KaVin S <kavinadithya3@gmail.com>
2024-06-26 23:05:16 +0530

    Area.java problem has been solved

---
+++ src/main/java/com/techcrack/DSA/Lesson3/Area.java
@@ -0,0 +1,24 @@
+package com.techcrack.DSA.Lesson3;
+import java.util.Scanner;
+import java.util.jar.JarEntry;
+
+public class Area {
+
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+
+//        System.out.print("Enter the Radius of the Circle : ");
+//        int radiusOfCircle = scanner.nextInt();
+//
+//        System.out.println("Area of circle is " + (radiusOfCircle * radiusOfCircle) + " for " + radiusOfCircle + " radius");
+
+        System.out.print("Enter the Height of triangle : ");
+        int height = scanner.nextInt();
+
+
+
+
+
+
+    }
+}
