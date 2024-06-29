4eb898a5746a74510cae66172d93e47b9a1e1dc3
KaVin S <kavinadithya3@gmail.com>
2024-06-29 18:47:36 +0530

    TrianglePattern.java problem solved

---
+++ src/main/java/com/techcrack/DSA/Patterns/TrianglePattern.java
@@ -0,0 +1,33 @@
+package com.techcrack.DSA.Patterns;
+
+public class TrianglePattern {
+    public static void printPattern(int length){
+
+        boolean decreases = true;
+        int starLength = length;
+        int spaceLength = 1;
+
+        while(starLength < length + 1){
+            for(int k = 1; k < spaceLength; k++)
+                System.out.print(" ");
+            for(int k = 0; k < starLength; k++)
+                System.out.print("*");
+            if(decreases){
+                starLength--;
+                spaceLength++;
+            }
+            else{
+                starLength++;
+                spaceLength--;
+            }
+
+            if(starLength == 1)
+                decreases = false;
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args){
+        printPattern(10);
+    }
+}
