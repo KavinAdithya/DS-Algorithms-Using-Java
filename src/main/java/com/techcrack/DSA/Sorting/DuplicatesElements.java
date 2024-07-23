b55456771675fabee787f185bd0e01968a7b4907
KaVin S <kavinadithya3@gmail.com>
2024-07-23 14:11:34 +0530

    Cyclic sort algorithm learning

---
+++ src/main/java/com/techcrack/DSA/Sorting/DuplicatesElements.java
@@ -0,0 +1,34 @@
+package com.techcrack.DSA.Sorting;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DuplicatesElements {
+    public List<Integer> findDuplicates(int[] nums) {
+        int length = nums.length;
+        int i = 0;
+
+        while (i < length) {
+            int correctIndex = nums[i] - 1;
+            if (nums[i] != nums[correctIndex])
+                swap(nums, i, correctIndex);
+            else
+                i++;
+        }
+
+        List<Integer> list = new ArrayList<>();
+
+        for (int j = 0; j < length; j++) {
+            if ( j  + 1 != nums[j])
+                list.add(nums[j]);
+        }
+
+        return list;
+    }
+
+    public void swap(int[] nums, int first, int last){
+        int temp = nums[first];
+        nums[first] = nums[last];
+        nums[last] = temp;
+    }
+}
