f738c91d7b745040a4eee5a1c6e6c4f7aab9d8f8
KaVin S <kavinadithya3@gmail.com>
2024-07-28 10:16:40 +0530

    Kthsamllest Fraction.java revisited

---
+++ src/main/java/com/techcrack/DSA/WeeklyContest/Week1/FindGreaterGame.java
@@ -0,0 +1,17 @@
+package com.techcrack.DSA.WeeklyContest.Week1;
+
+public class FindGreaterGame {
+    public boolean canAliceWin(int[] nums) {
+        int singleDigit = 0;
+        int doubleDigit = 0;
+
+        for (int num : nums) {
+            if (num <= 9)
+                singleDigit += num;
+            else
+                doubleDigit += num;
+        }
+
+        return singleDigit != doubleDigit;
+        }
+}
