1cdc87a16f1814954666f82517ada3316a54a476
KaVin S <kavinadithya3@gmail.com>
2024-07-28 11:16:24 +0530

    CountNotSpecial.java weekly contest problem learned and solved

---
+++ src/main/java/com/techcrack/DSA/WeeklyContest/Week1/CountNotSpecial.java
@@ -0,0 +1,66 @@
+package com.techcrack.DSA.WeeklyContest.Week1;
+
+class CountNotSpecial {
+    public int nonSpecialCount1(int l, int r) {
+        int notSpecial = 0;
+
+        for (int i = l; i < r + 1; i++) {
+            if ((i != 2 && i != 4 && i % 2 == 0) || (i != 5 && i % 5 == 0)) {
+                notSpecial++;
+                continue;
+            }
+
+            if(findCount(i))
+                notSpecial++;    
+        }
+
+        return notSpecial;
+    }
+
+    public boolean findCount(int target) {
+        int divisors = 0;
+
+        for (int i = 1; i < target; i++) {
+            if (target % i == 0)
+                divisors++;
+            if (divisors > 2)
+                return true;
+        }
+
+        return divisors < 2;
+    }
+
+
+    // Sieve of Eratosthenes
+    public int nonSpecialCount(int l, int r) {
+        int limit = (int) Math.sqrt(r);
+
+        boolean[] isPrime = new boolean[limit + 1];
+
+        isPrime[0] = isPrime[1] = true;
+
+        for (int i = 2; i * i < limit + 1; i++) {
+            if (! isPrime[i]) {
+                for (int j = i * i; j < limit + 1; j += i) {
+                    isPrime[j] = true;
+                }
+            }
+        }
+
+        int specialElements = 0;
+
+        for (int i = 2; i < limit + 1; i++) {
+            if (!isPrime[i]) {
+                int range = i * i;
+
+                if (range >= l && range <= r)
+                    specialElements++;
+            }
+        }
+
+        int totalElements = r - l + 1;
+
+        return totalElements - specialElements;
+
+    }
+}
