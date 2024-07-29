0139e664ee899efbe133593ec2857eebdc75130c
KaVin S <kavinadithya3@gmail.com>
2024-07-29 17:31:32 +0530

    CountItemsMatchingRule.java , CountItemsMatchingRule.java and SortingSentences problems Solved

---
+++ src/main/java/com/techcrack/DSA/Strings/CountItemsMatchingRule.java
@@ -0,0 +1,71 @@
+package com.techcrack.DSA.Strings;
+
+import java.util.List;
+
+class CountItemsMatchingRule {
+    public int countMatches1(List<List<String>> items, String ruleKey, String ruleValue) {
+        int index = 2;
+
+        if (ruleKey.equals("type"))
+            index = 0;
+        else if (ruleKey.equals("color"))
+            index  = 1;
+ 
+        return findCount(items, index, ruleValue);
+        
+    }
+
+    public int findCount(List<List<String>> items, int index, String value) {
+        int answer = 0;
+
+        for (List<String> item : items) {
+            if (item.get(index).equals(value))
+                answer++;
+        }
+
+        return answer;
+    }
+
+    public int countMatches2(List<List<String>> items, String ruleKey, String ruleValue) {
+        int answer = 0;
+        boolean type = false;
+        boolean color = false;
+        boolean name = false;
+
+        if (ruleKey.equals("type"))
+            type = true;
+        else if (ruleKey.equals("color"))
+            color = true;
+        else
+            name = true;
+
+
+        for (List<String> item : items) {
+            if (type && item.get(0).equals(ruleValue))
+                answer++;
+            else if (color && item.get(1).equals(ruleValue))
+                answer++;
+            else if (name && item.get(2).equals(ruleValue))
+                answer++;
+        }
+
+        return answer;
+
+    }
+
+    public int countMatches(List<List<String>> items, String ruleKey, String ruleValue) {
+        int answer = 0;
+
+        for (List<String> item : items) {
+            if (ruleKey.equals("type") && item.get(0).equals(ruleValue))
+                answer++;
+            else if (ruleKey.equals("color") && item.get(1).equals(ruleValue))
+                answer++;
+            else if (ruleKey.equals("name") && item.get(2).equals(ruleValue))
+                answer++;
+        }
+
+        return answer;
+
+    }
+}
---
+++ src/main/java/com/techcrack/DSA/WeeklyContest/Week1/CountSubStrings.java
@@ -0,0 +1,86 @@
+package com.techcrack.DSA.WeeklyContest.Week1;
+
+public class CountSubStrings {
+
+    // Brute Force
+    public int numberOfSubstrings1(String s) {
+        int satisfiedString = 0;
+        int length = s.length();
+
+        for (int i = 0; i < length; i++) {
+            long ones = 0;
+            long zeros = 0;
+
+            for (int j = i; j < length; j++) {
+                char binaryCharacter = s.charAt(j);
+
+                if (binaryCharacter == '0')
+                    zeros++;
+                else
+                    ones++;
+
+                if (ones >= zeros * zeros)
+                    satisfiedString++;
+            }
+        }
+
+        return satisfiedString;
+    }
+
+    public int numberOfSubstrings(String s) {
+        if (s == null || s.isEmpty())
+            return 0;
+
+        int n = s.length();
+
+        int[] prefixSum = new int[n + 1];
+
+        findPrefixSum(s, prefixSum);
+
+        return countSubStringWithDominant(s, prefixSum, n);
+    }
+
+    public void findPrefixSum(String s, int[] prefixSum) {
+        if (s.charAt(0) == '1')
+            prefixSum[1] = 1;
+
+        for (int i = 1; i < s.length() ; i++) {
+            if (s.charAt(i) == '1')
+                prefixSum[i + 1] = prefixSum[i] + 1;
+            else
+                prefixSum[i + 1] = prefixSum[i];
+        }
+    }
+
+    public int countSubStringWithDominant(String s, int[] prefixSum, int length) {
+        int ans = 0;
+
+        for (int i = 0; i < length; i++) {
+            for (int j = i; j < length; j++) {
+                int ones = prefixSum[j + 1] - prefixSum[i];
+                int zeros = (j - i + 1) - ones;
+                int zerosSquare = zeros * zeros;
+
+                if (zerosSquare > ones)
+                    j += (zerosSquare - ones - 1);
+                else if (zerosSquare == ones)
+                    ans++;
+                else {
+                    int differenceDigits = (int)Math.sqrt(ones) - zeros;
+                    int totalDifference = differenceDigits + j;
+
+                    if (totalDifference >= length)
+                        ans += (length - j);
+                    else
+                        ans += differenceDigits + 1;
+
+                    j = totalDifference;
+
+                }
+
+            }
+        }
+
+        return ans;
+    }
+}
