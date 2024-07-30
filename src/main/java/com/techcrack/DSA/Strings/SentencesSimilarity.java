62ee6571a229452d7a2c31d0d021d6cd63bf7dd8
KaVin S <kavinadithya3@gmail.com>
2024-07-30 12:50:00 +0530

    SentencesSimilarity.java problem solved

---
+++ src/main/java/com/techcrack/DSA/Strings/SentencesSimilarity.java
@@ -0,0 +1,70 @@
+package com.techcrack.DSA.Strings;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class SentencesSimilarity {
+    public boolean areSentencesSimilar1(String sentence1, String sentence2) {
+      
+        String[] words1 = sentence1.split(" ");
+        String[] words2 = sentence2.split(" ");
+        
+        if (words1.length < words2.length)
+            return areSentencesSimilar1(sentence2, sentence1);
+
+        if (words1.length == words2.length)
+            return sentence1.equals(sentence2);
+
+
+        List<Integer> missingIndex = new ArrayList<>();
+        List<Integer> foundedIndex = new ArrayList<>();
+        int j = 0;
+
+        for (int i = 0; i < words1.length; i++) {
+            String s1 = words1[i];
+            String s2 = j < words2.length ? words2[j] : null;
+
+            if (s2 == null)
+                missingIndex.add(i);
+            else if(! s1.equals(s2)) {
+                missingIndex.add(i);
+                j--;
+            }
+            else
+                foundedIndex.add(i);
+            j++;
+        }
+
+        System.out.println(j  + " " + missingIndex + " " + foundedIndex);
+        if (missingIndex.size() + j == words1.length && j  != words2.length)
+            return false;
+        
+
+        for (int i = 1; i < missingIndex.size(); i++)
+            if (missingIndex.get(i) - missingIndex.get(i - 1) != 1)
+                return false;
+        
+        return true;
+    }
+
+    public boolean areSentencesSimilar(String sentence1, String sentence2) {
+        String[] words1 = sentence1.split(" ");
+        String[] words2 = sentence2.split(" ");
+
+        int length1 = words1.length;
+        int length2 = words2.length;
+
+        if (length1 > length2)
+            return areSentencesSimilar(sentence2, sentence1);
+
+        int i = 0;
+
+        while (i < length1 && words1[i].equals(words2[i]))
+            i++;
+
+        while (i < length1 && words1[i].equals(words2[i + length2 - length1]))
+            i++;
+
+        return i == length1;
+    }
+}
