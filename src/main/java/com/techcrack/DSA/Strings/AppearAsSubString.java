573904b3543c048036db43633ee72ff3b033cf26
KaVin S <kavinadithya3@gmail.com>
2024-08-03 05:30:56 +0530

    AppearAsSubString.java problem solved

---
+++ src/main/java/com/techcrack/DSA/Strings/AppearAsSubString.java
@@ -0,0 +1,14 @@
+package com.techcrack.DSA.Strings;
+
+class AppearAsSubString {
+    public int numOfStrings(String[] patterns, String word) {
+        int count = 0;
+        
+        for (String pattern : patterns) {
+            if (word.contains(pattern))
+                count++;
+        }
+
+        return count;
+    }
+}
