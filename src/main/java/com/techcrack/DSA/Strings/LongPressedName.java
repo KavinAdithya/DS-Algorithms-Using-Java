8cc0d8143ac2f9ba3a8720b7c2366671ef7a8aac
KaVin S <kavinadithya3@gmail.com>
2024-08-10 04:17:30 +0530

    LongPressedName.java problem solved

---
+++ src/main/java/com/techcrack/DSA/Strings/LongPressedName.java
@@ -0,0 +1,38 @@
+package com.techcrack.DSA.Strings;
+
+class LongPressedName {
+    public boolean isLongPressedName(String name, String typed) {
+        
+        if (name == null || typed == null)
+            return false;
+
+        char[] nameArray = name.toCharArray();
+        char[] typedArray = typed.toCharArray();
+
+        int length = name.length();
+        int typedLength = typed.length();
+
+        int namePointer = 0;
+        int typedPointer = 0;
+
+        if (length > typedLength)
+            return false;
+
+        while (typedPointer < typedLength ) {
+            char ch = namePointer >= length ? nameArray[length - 1] : nameArray[namePointer];
+            char ch1 = typedArray[typedPointer];
+
+            if (ch == ch1) {
+                namePointer++;
+                typedPointer++;
+            }
+            else if (namePointer != 0 && ch1 == typedArray[typedPointer - 1]) 
+                typedPointer++;
+            else
+                return false;
+        }
+
+
+        return namePointer >= length;
+    }
+}
