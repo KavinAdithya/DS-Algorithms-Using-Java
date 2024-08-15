bfbe970c552fe66824bff4f76579ac53e6e438bc
KaVin S <kavinadithya3@gmail.com>
2024-08-15 10:30:01 +0530

    MaximumRepeating.java problem solved

---
+++ src/main/java/com/techcrack/DSA/Strings/MaximumRepeating.java
@@ -0,0 +1,19 @@
+package com.techcrack.DSA.Strings;
+
+class MaximumRepeating {
+    public int maxRepeating(String sequence, String word) {
+    
+        int length = sequence.length();
+        StringBuilder str = new StringBuilder(word);
+
+
+        for (int i = 0; i < length; i++) {
+            if (sequence.contains(str.toString()))
+                str.append(word);
+            else
+               return i ;
+        }
+
+        return length;
+    }
+}
