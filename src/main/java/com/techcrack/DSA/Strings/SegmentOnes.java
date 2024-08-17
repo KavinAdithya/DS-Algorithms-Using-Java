6bb004b227ca8aea80fda3027f3733d22270c547
KaVin S <kavinadithya3@gmail.com>
2024-08-18 04:03:17 +0530

    SegmentOnes.java problem solved.

--- src/main/java/com/techcrack/DSA/Strings/SegmentOnes.java
+++ src/main/java/com/techcrack/DSA/Strings/SegmentOnes.java
@@ -28,4 +28,32 @@ class SegmentOnes {
         
         return segmentOnes < 2;
     }
+
+
+    // Time Complexity -> 0(n)
+    // Space Complexity -> 0(1)
+    public boolean checkOnesSegment1(String s) {
+
+        for (int i = 1; i < s.length() - 1; i++) {
+            if (s.charAt(i) == '0' && s.charAt(i + 1) == '1')
+                return false;
+        }
+
+        return true;
+    }
+
+
+    public boolean checkOnesSegment2(String s) {
+        int segmentOnes = 1;
+        int segmentSum = 0;
+
+        for (int i = 1; i < s.length() - 1; i++) {
+            char ch = s.charAt(i);
+
+            if (ch == '0' && s.charAt( i + 1) == '1')
+                segmentOnes++;
+        }
+
+        return segmentOnes < 2;
+    }
 }
