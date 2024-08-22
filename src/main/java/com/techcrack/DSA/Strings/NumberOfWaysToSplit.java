de70cadd90c20e52eb7c816b96c8b82d173a6f07
KaVin S <kavinadithya3@gmail.com>
2024-08-23 04:03:48 +0530

    NumberOfWaysToSplit.java problem Revisited

--- src/main/java/com/techcrack/DSA/Strings/NumberOfWaysToSplit.java
+++ src/main/java/com/techcrack/DSA/Strings/NumberOfWaysToSplit.java
@@ -1,7 +1,7 @@
 package com.techcrack.DSA.Strings;
 
 public class NumberOfWaysToSplit {
-    public int numWays(String s) {
+    public int numWays1(String s) {
         long MODULO = 1_000_000_007;
         long countOnes = 0;
         long n = s.length();
@@ -32,5 +32,42 @@ public class NumberOfWaysToSplit {
 
 
         return (int) (firstGap * secondGap % MODULO);
+    }
+
+
+    public int numWays(String s) {
+        long MOD = 1_000_000_007;
+        int ones = 0;
+
+
+        long n = s.length();
+
+        for (int i = 0; i < n; i++) {
+            ones += s.charAt(i) - '0';
+        }
+
+        if (ones == 0)
+            return (int) ((n - 1) * (n - 2) / 2 % MOD);
+
+        if (ones % 3 != 0)
+            return 0;
+
+        long equalParts = ones / 3;
+        long sumParts = 0;
+        long firstCuts = 0;
+        long secondCuts = 0;
+
+        for (int i = 0; i < n; i++) {
+            sumParts += s.charAt(i) - '0';
+
+            if (sumParts == equalParts)
+                firstCuts++;
+
+            if (sumParts == 2 * equalParts)
+                secondCuts++;
+        }
+
+        return (int)(firstCuts * secondCuts % MOD);
+
     }
 }
