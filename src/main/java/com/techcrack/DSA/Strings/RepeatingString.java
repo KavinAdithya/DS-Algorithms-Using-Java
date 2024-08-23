e40f58b3f716e959499d3b47076adba3a5b4b93a
KaVin S <kavinadithya3@gmail.com>
2024-08-24 04:41:16 +0530

    RepeatingString.java problem Revisited

--- src/main/java/com/techcrack/DSA/Strings/RepeatingString.java
+++ src/main/java/com/techcrack/DSA/Strings/RepeatingString.java
@@ -1,7 +1,7 @@
 package com.techcrack.DSA.Strings;
 
 class RepeatingString {
-    public int repeatedStringMatch(String a, String b) {
+    public int repeatedStringMatch2(String a, String b) {
         boolean[] bucket = new boolean[26];
 
         for (char ch : a.toCharArray())
@@ -40,6 +40,62 @@ class RepeatingString {
         if (s.indexOf(b) != -1)
             return count;
         
+        return -1;
+    }
+
+
+    public int repeatedStringMatch3(String a, String b) {
+        StringBuilder word1 = new StringBuilder();
+
+        boolean[] bucket = new boolean[26];
+
+        for (int i = 0; i < a.length(); i++)
+            bucket[a.charAt(i) - 'a'] = true;
+
+        for (int i = 0; i < b.length(); i++)
+            if (! bucket[b.charAt(i) - 'a'])
+                return -1;
+
+
+        int length =  (int) Math.ceil( (double) b.length()  / a.length());
+        int i = 0;
+
+        while (i < length) {
+            word1.append(a);
+            //System.out.println(word1 + " " + word1.indexOf(b));
+
+            if (word1.indexOf(b) != -1)
+                return i + 1;
+            i++;
+        }
+
+        word1.append(a);
+
+        return word1.indexOf(b) != -1 ? i + 1 : -1;
+    }
+
+
+    public int repeatedStringMatch(String a, String b) {
+        boolean[] bucket = new boolean[26];
+
+        for (char ch : a.toCharArray())
+            bucket[ch - 'a'] = true;
+
+        for (char ch : b.toCharArray()) {
+            if (! bucket[ch - 'a'])
+                return -1;
+        }
+
+        int length = b.length() / a.length();
+
+        StringBuilder sb = new StringBuilder(a.repeat(length));
+
+        for (int i = 0; i < 3; i ++) {
+            if (sb.indexOf(b) != -1)
+                return length + i;
+            sb.append(a);
+        }
+
         return -1;
     }
 }
