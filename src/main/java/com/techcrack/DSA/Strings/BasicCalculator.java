77e2b88d62dc3f8db0c452191b9db3e637d683ef
KaVin S <kavinadithya3@gmail.com>
2024-08-26 05:22:58 +0530

    BasicCalculator.java problem revisited

--- src/main/java/com/techcrack/DSA/Strings/BasicCalculator.java
+++ src/main/java/com/techcrack/DSA/Strings/BasicCalculator.java
@@ -3,7 +3,7 @@ package com.techcrack.DSA.Strings;
 import java.util.Stack;
 
 class BasicCalculator {
-    public int calculate(String s) {
+    public int calculate2(String s) {
         char operator = '+';
         int number =  0;
         Stack<Integer> numberStack = new Stack<>();
@@ -38,5 +38,92 @@ class BasicCalculator {
             sum += numberStack.pop();
         
         return sum;
+    }
+    public int calculate1(String s) {
+        int length = s.length();
+
+        Stack<Integer> operand = new Stack<>();
+        Stack<Character> operator = new Stack<>();
+
+        int digit = 0;
+
+        int sPointer = 0;
+
+        while (sPointer < length) {
+            char ch = s.charAt(sPointer);
+
+            if (Character.isDigit(ch))
+                digit = (digit * 10) + (ch - '0');
+
+            if ( ( ! Character.isDigit(ch) && ch != ' ') || sPointer + 1 == length ) {
+                if ( ! operator.isEmpty() && operator.peek() == '-')
+                    digit = digit * -1;
+
+                operand.push(digit);
+                digit = 0;
+
+                if ( ! operator.isEmpty() && (operator.peek() == '/' || operator.peek() == '*') ) {
+                    int b = operand.pop();
+                    int a = operand.pop();
+
+                    int c = operator.pop() == '*' ? a * b: a / b;
+
+                    operand.push(c);
+                }
+
+                if (ch != ' ' && ! Character.isDigit(ch))
+                    operator.push(ch);
+            }
+            sPointer++;
+        }
+
+
+
+
+        int sum = 0;
+
+        while ( ! operand.isEmpty() )
+            sum += operand.pop();
+
+
+        return sum;
+    }
+
+    public int calculate3(String s) {
+        int length = s.length();
+        char operator = '+';
+
+        int number = 0;
+
+        Stack<Integer> operand = new Stack<>();
+
+        for (int i = 0; i < length; i++) {
+            char ch = s.charAt(i);
+
+            if (Character.isDigit(ch))
+                number = (number * 10) + (ch - '0');
+
+
+            if ( ( ! Character.isDigit(ch) && ch != ' ') || i + 1 == length ) {
+                if (operator == '+')
+                    operand.push(number);
+                else if (operator == '-')
+                    operand.push(number * -1);
+                else if (operator == '*')
+                    operand.push(operand.pop() * number);
+                else
+                    operand.push(operand.pop() / number);
+
+                number = 0;
+                operator = ch;
+            }
+        }
+
+        int result = 0;
+
+        while ( ! operand.isEmpty() )
+            result += operand.pop();
+
+        return result;
     }
 }
