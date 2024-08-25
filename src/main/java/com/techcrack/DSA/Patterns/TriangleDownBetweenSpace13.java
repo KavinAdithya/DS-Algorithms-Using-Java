2eb2aa1ec3b67b78b99d3eb6b58e8dd2383410a3
KaVin S <kavinadithya3@gmail.com>
2024-08-25 17:08:11 +0530

    TriangleDownBetweenSpace13 pattern solved

---
+++ src/main/java/com/techcrack/DSA/Patterns/TriangleDownBetweenSpace13.java
@@ -0,0 +1,26 @@
+package com.techcrack.DSA.Patterns;
+
+public class TriangleDownBetweenSpace13 {
+
+    static void triangleDownBetweenSpace(int length) {
+        for (int i = 0; i < length; i++) {
+            for (int j = 0; j < length - i; j++)
+                System.out.print(" ");
+            for (int k = 0; k < i + 1; k++) {
+                if (k == 0 || k == i)
+                    System.out.print("* ");
+                else
+                    System.out.print("  ");
+            }
+
+            System.out.println();
+        }
+
+        for (int i = 0; i < length * 2 + 1; i++)
+            System.out.print("*");
+    }
+
+    public static void main(String[] args) {
+        triangleDownBetweenSpace(10);
+    }
+}
