3cfcf99d62238e05988bf897e909800a0936350a
KaVin S <kavinadithya3@gmail.com>
2024-08-25 16:59:06 +0530

    ExcelColumnTitle.java problem revisited

--- src/main/java/com/techcrack/DSA/Strings/ExcelColumnTitle.java
+++ src/main/java/com/techcrack/DSA/Strings/ExcelColumnTitle.java
@@ -47,4 +47,18 @@ class ExcelColumnTitle {
             }
         }
     }
+
+    public String convertToTitle(int columnNumber) {
+        StringBuilder result = new StringBuilder();
+
+        while (columnNumber > 0) {
+            columnNumber--;
+
+            result.append((char)('A' + columnNumber % 26));
+
+            columnNumber /= 26;
+        }
+
+        return result.reverse().toString();
+    }
 }
