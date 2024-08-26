03ab2b90db622c9369dc586b378245a2ecd7bddc
KaVin S <kavinadithya3@gmail.com>
2024-08-26 07:30:43 +0530

    GoodNodes.java problem revisited

--- src/main/java/com/techcrack/DSA/WeeklyContest/Week410/GoodNodes.java
+++ src/main/java/com/techcrack/DSA/WeeklyContest/Week410/GoodNodes.java
@@ -6,8 +6,10 @@ class GoodNodes {
     private List<List<Integer>> graph;
     private boolean[] visited;
     private int[] childCount;
+    boolean[] visitedNodes;
+    int[] childrens;
 
-    public int countGoodNodes(int[][] edges) {
+    public int countGoodNodes1(int[][] edges) {
         int length = edges.length + 1;
 
         graph = new ArrayList<>(length);
@@ -23,7 +25,7 @@ class GoodNodes {
             graph.get(edge[1]).add(edge[0]);
         }       
 
-        dfs(0);
+        dfs1(0);
 
         System.out.println(Arrays.toString(childCount));
 
@@ -31,13 +33,13 @@ class GoodNodes {
     }
 
 
-    public int dfs(int node) {
+    public int dfs1(int node) {
         visited[node] = true;
         int size = 1;
 
         for (int child : graph.get(node)) {
             if (!visited[child]) 
-                size += dfs(child);
+                size += dfs1(child);
         }
 
         childCount[node] = size;
@@ -69,5 +71,64 @@ class GoodNodes {
         }
 
         return goodNodes;
+    }
+
+
+    public int countGoodNodes(int[][] edges) {
+        int length = edges.length + 1;
+
+        graph = new ArrayList<>(length);
+        visitedNodes = new boolean[length];
+        childrens = new int[length];
+
+        for (int i = 0; i < length; i++)
+            graph.add(new ArrayList<>());
+
+        for (int[] edge : edges) {
+            graph.get(edge[0]).add(edge[1]);
+            graph.get(edge[1]).add(edge[0]);
+        }
+
+        dfs(0);
+
+        // System.out.println(Arrays.toString(childrens));
+
+        int goodNodes = 0;
+
+        for (int i = 0; i < length; i++) {
+
+            List<Integer> ls = graph.get(i);
+            int previousNode = -1;
+            boolean isGood = true;
+
+            for (int node : ls) {
+
+                if (previousNode == -1 && childrens[i] > childrens[node])
+                    previousNode = childrens[node];
+                else if (previousNode != childrens[node] && previousNode != -1) {
+                    isGood = false;
+                    break;
+                }
+
+            }
+
+            if (isGood)
+                goodNodes++;
+        }
+
+        return goodNodes;
+    }
+
+    private int dfs(int index) {
+        visitedNodes[index] = true;
+        int size = 1;
+
+        for (int node : graph.get(index)) {
+            if ( !visitedNodes[node])
+                size += dfs(node);
+        }
+
+        childrens[index] = size;
+        return size;
     }
 }
