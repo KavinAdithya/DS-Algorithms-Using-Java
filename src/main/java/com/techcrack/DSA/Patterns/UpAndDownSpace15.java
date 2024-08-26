a10e0248afdbb4de085861593a2b5dcc2e75eb44
KaVin S <kavinadithya3@gmail.com>
2024-08-26 08:00:40 +0530

    UpAndDownSpace15 problem solved

---
+++ src/main/java/com/techcrack/DSA/Patterns/UpAndDownSpace15.java
@@ -0,0 +1,38 @@
+package com.techcrack.DSA.Patterns;
+
+public class UpAndDownSpace15 {
+    static void upAndDownSpace(int length ) {
+        for (int i = 0; i < length; i++) {
+            for (int j = i; j < length ; j++)
+                System.out.print(" ");
+            for (int k = 0; k < i + 1; k++) {
+                if (k == 0 || k == i )
+                    System.out.print("* ");
+                else
+                    System.out.print("  ");
+
+            }
+
+            System.out.println();
+        }
+
+        for (int i = 0; i < length - 1; i++) {
+            for (int j = 0; j < i + 2; j++)
+                System.out.print(" ");
+            for (int k = length - i - 1; k > 0 ; k--) {
+                if (k == length  - i - 1 || k == 1 )
+                    System.out.print("* ");
+                else
+                    System.out.print("  ");
+
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        upAndDownSpace(5);
+
+    }
+
+}
