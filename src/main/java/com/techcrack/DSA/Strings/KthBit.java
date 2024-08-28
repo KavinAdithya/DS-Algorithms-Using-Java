8df5f83a9e4aad7c44ab6fefa17964c1ef7d1b22
KaVin S <kavinadithya3@gmail.com>
2024-08-29 04:46:53 +0530

    KthBit.java problem revisited

--- src/main/java/com/techcrack/DSA/Strings/KthBit.java
+++ src/main/java/com/techcrack/DSA/Strings/KthBit.java
@@ -36,7 +36,7 @@ class KthBit {
     // Optimal recursion solution
     // Time Complexity -> 0(n)
     // Space Complexity -> 0(1)
-    public char findKthBit(int n, int k) {
+    public char findKthBit2(int n, int k) {
         k--;
         return findBit(n, k);
     }
@@ -56,5 +56,57 @@ class KthBit {
             return '1';
         else
             return findBit(n - 1, length - k - 1 ) == '0' ? '1' : '0';
+    }
+
+    public char findKthBit3(int n, int k) {
+        StringBuilder result = new StringBuilder("0");
+
+        for (int i = 0; i < n; i++) {
+            result.append('1');
+            reverseInvert(result);
+
+            if (result.length() >= k)
+                break;
+        }
+
+        return result.charAt( k - 1);
+    }
+
+    private void reverseInvert(StringBuilder result) {
+        int length = result.length();
+
+        for (int i = length - 2; i > -1; i--) {
+            if (result.charAt(i) == '0')
+                result.append('1');
+            else
+                result.append('0');
+        }
+
+
+    }
+
+    public char findKthBit4(int n, int k) {
+        return bitOfBinaryString(n, k - 1);
+    }
+
+    private char bitOfBinaryString(int n , int k) {
+
+
+        if (n == 1)
+            return '0';
+
+        int length = (int) Math.pow(2, n) - 1;
+        int mid = length / 2;
+
+        if (mid == k)
+            return '1';
+        else if (k < mid)
+            return bitOfBinaryString(n - 1, k);
+        else
+            return invert(bitOfBinaryString(n - 1, length - k - 1));
+    }
+
+    private char invert(char ch) {
+        return ch == '1' ? '0' : '1';
     }
 }
