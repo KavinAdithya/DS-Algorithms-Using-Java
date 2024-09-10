79a579f62632cb9d0af216a0f5409a83e10eae05
KaVin S <kavinadithya3@gmail.com>
2024-09-10 05:35:16 +0530

    TransformableSortSubstring.java problem learned and solved. OperationSorted.java problem brute force solution solved.

---
+++ src/main/java/com/techcrack/DSA/Strings/TransformableSortSubstring.java
@@ -0,0 +1,38 @@
+package com.techcrack.DSA.Strings;
+
+import java.util.*;
+
+class TransformableSortSubstring {
+    public boolean isTransformable(String s, String t) {
+        List<Deque<Integer>> positions = new ArrayList<>();
+
+        for (int i = 0; i < 10; i++) {
+            positions.add(new ArrayDeque<>());
+        }
+
+        for (int i = 0; i < s.length(); i++) {
+            int digit = s.charAt(i) - '0';
+            positions.get(digit).add(i);
+        }
+
+
+        for (int i = 0; i < t.length(); i++) {
+            int targetElement = t.charAt(i) - '0';
+
+            if (positions.get(targetElement).isEmpty())
+                return false;
+            
+            int currentIndex = positions.get(targetElement).peek();
+
+            for (int j = 0; j < targetElement; j++) {
+                if (! positions.get(j).isEmpty() && positions.get(j).peek() < currentIndex)
+                    return false;
+            }
+
+            positions.get(targetElement).poll();
+        }
+
+
+        return true;
+    }
+}
