12af8dbb0f3e082b6c1e6808b16fd867ef82db34
KaVin S <kavinadithya3@gmail.com>
2024-09-14 16:39:11 +0530

    HollowRectanglePattern.java problem solved

---
+++ src/main/java/com/techcrack/DSA/Patterns/HollowRectanglePattern.java
@@ -0,0 +1,36 @@
+package com.techcrack.DSA.Patterns;
+
+import java.awt.desktop.SystemEventListener;
+
+public class HollowRectanglePattern {
+
+    static void hollowRectanglePattern(int row, int column) {
+        printLine(column);
+
+        printSpace(row, column);
+
+        printLine(column);
+    }
+
+    static void printLine(int column) {
+        for (int i = 1; i < column + 1; i++)
+            System.out.print("*");
+
+        System.out.println();
+    }
+
+    static void printSpace(int row, int column) {
+        for (int i = 1; i < row - 1; i++) {
+            System.out.print("*");
+
+            for (int j = 1; j < column - 1; j++)
+                System.out.print(" ");
+
+            System.out.println("*");
+        }
+    }
+
+    public static void main(String[] args) {
+        hollowRectanglePattern(5, 10);
+    }
+}
