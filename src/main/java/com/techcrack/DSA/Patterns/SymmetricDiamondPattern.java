68f8bcf190881463055aef1d4c708c5dbb457a4f
KaVin S <kavinadithya3@gmail.com>
2024-09-14 15:46:06 +0530

    SymmetricDiamondPattern.java pattern solved

---
+++ src/main/java/com/techcrack/DSA/Patterns/SymmetricDiamondPattern.java
@@ -0,0 +1,42 @@
+package com.techcrack.DSA.Patterns;
+
+public class SymmetricDiamondPattern {
+    public static void symmetricDiamond(int length) {
+        printTop(length);
+        printBottom(length);
+    }
+
+    private static void printTop(int length) {
+        for (int i = 1; i < length + 1; i++) {
+            for (int j = 1; j < length - i + 2; j++)
+                System.out.print(" ");
+
+            for (int j = i; j > 0; j--)
+                System.out.print(j);
+
+            for (int j = 2; j < i + 1; j++)
+                System.out.print(j);
+
+            System.out.println();
+        }
+    }
+
+    private static void printBottom(int length) {
+        for (int i = 1; i < length; i++) {
+            for (int j = 1; j < i + 2; j++)
+                System.out.print(" ");
+
+            for (int j = length - i; j > 0; j--)
+                System.out.print(j);
+
+            for (int j = 2; j < length - i + 1; j++)
+                System.out.print(j);
+
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        symmetricDiamond(4);
+    }
+}
