bc4080e072de0385db1f0f3ea5688686b8fbd0d2
KaVin S <kavinadithya3@gmail.com>
2024-09-17 05:47:52 +0530

    Pangram.java problem solved

---
+++ src/main/java/com/techcrack/DSA/Strings/Pangram.java
@@ -0,0 +1,18 @@
+package com.techcrack.DSA.Strings;
+
+class Pangram {
+    public boolean checkIfPangram(String sentence) {
+        int[] alphaHash = new int[26];
+
+        for (int i = 0; i < sentence.length(); i++)
+            alphaHash[sentence.charAt(i) - 'a']++;
+
+        for (int i = 0; i < 26; i++) {
+            if (alphaHash[i] == 0)
+                return false;
+        }
+
+        return true;
+
+    }
+}
