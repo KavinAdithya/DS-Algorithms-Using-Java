59f2755490faf0a580ae32c2e565f54d7a3b2395
KaVin S <kavinadithya3@gmail.com>
2024-10-22 05:56:05 +0530

    PartitionArray.java problem solved

---
+++ src/main/java/com/techcrack/DSA/Sorting/PartitionArray.java
@@ -0,0 +1,16 @@
+package com.techcrack.DSA.Sorting;
+
+import java.util.Arrays;
+
+class PartitionArray {
+    public int arrayPairSum(int[] nums) {
+        Arrays.sort(nums);
+
+        int sum = 0;
+
+        for (int i = 0; i < nums.length; i += 2)
+            sum += nums[i];
+        
+        return sum;
+    }
+}
