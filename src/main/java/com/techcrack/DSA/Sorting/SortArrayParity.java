9a8741e101dd26e705c4b9454dc5dd88a031b205
KaVin S <kavinadithya3@gmail.com>
2024-10-23 17:25:38 +0530

    ThreeSumClosest.java problem solved

---
+++ src/main/java/com/techcrack/DSA/Sorting/SortArrayParity.java
@@ -0,0 +1,26 @@
+package com.techcrack.DSA.Sorting;
+
+class SortArrayParity {
+    public int[] sortArrayByParity(int[] nums) {
+       int i = 0;
+       int j = nums.length - 1;
+
+       while (i < j) {
+            while (i < j && nums[i] % 2 == 0)
+                i++;
+            while (i < j && nums[j] % 2 != 0)
+                j--;
+            
+            if (i <= j) {
+                int temp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = temp;
+                i++;
+                j--;
+            }
+       }
+
+       return nums;
+
+    }
+}
