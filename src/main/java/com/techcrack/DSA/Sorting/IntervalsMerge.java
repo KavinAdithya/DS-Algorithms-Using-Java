0efb73beabccd78bbb08c52ddb62397cd3861f9b
KaVin S <kavinadithya3@gmail.com>
2024-10-30 12:41:26 +0530

    AbsoluteMinimumDifference.java problem solved

--- src/main/java/com/techcrack/DSA/Sorting/IntervalsMerge.java
+++ src/main/java/com/techcrack/DSA/Sorting/IntervalsMerge.java
@@ -42,4 +42,30 @@ class IntervalsMerge {
         return mergedArray;
 
     }
+
+    public int[][] merge(int[][] intervals) {
+
+        if (intervals == null || intervals.length < 2)
+            return intervals;
+
+        Arrays.sort(intervals, (a, b) -> a[0] - b[0]);
+
+        List<int[]> mergedIntervals = new ArrayList<>();
+        int[] currentInterval = intervals[0];
+
+        for (int i = 1; i < intervals.length; i++) {
+            int[] interval = intervals[i];
+
+
+            if (interval[0] > currentInterval[1]) {
+                mergedIntervals.add(currentInterval);
+                currentInterval = interval;
+            }
+            else
+                currentInterval[1] = Math.max(interval[1], currentInterval[1]);
+        }
+
+        mergedIntervals.add(currentInterval);
+        return mergedIntervals.toArray(new int[mergedIntervals.size()][]);
+    }
 }
