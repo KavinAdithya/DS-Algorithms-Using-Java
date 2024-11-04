621834478593a618cbc962ebc4929f45250f1de4
KaVin S <kavinadithya3@gmail.com>
2024-11-04 06:22:42 +0530

    Learn About Floyd's Cycle detection algorithm and solve the Largest Number problem

---
+++ src/main/java/com/techcrack/DSA/Algorithms/HareTortoiseAlgorithm.java
@@ -0,0 +1,31 @@
+package com.techcrack.DSA.Algorithms;
+
+import java.util.Arrays;
+
+public class HareTortoiseAlgorithm {
+
+    static int findDuplicates(int[] arr) {
+        int hare = arr[0];
+        int tortoise = arr[arr[0]];
+
+        while (hare != tortoise) {
+            hare = arr[hare];
+            tortoise = arr[arr[tortoise]];
+        }
+
+        hare = arr[0];
+
+        while (hare != tortoise) {
+            hare = arr[hare];
+            tortoise = arr[tortoise];
+        }
+
+//        Arrays.sort(new int[]{1, 2,3, 4}, ((a,b) -> Integer.compare((Integer) a, b))) ;
+        return hare;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5, 5, 6};
+        System.out.println(findDuplicates(arr));
+    }
+}
