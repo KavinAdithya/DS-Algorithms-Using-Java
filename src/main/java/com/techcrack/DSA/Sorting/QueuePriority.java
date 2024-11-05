2eb5f72be3ced74d0e872060e95db454d69eb192
KaVin S <kavinadithya3@gmail.com>
2024-11-06 04:51:54 +0530

    Solve KthLargestNumber problem and learn about quick select from quick sort algorithm

---
+++ src/main/java/com/techcrack/DSA/Sorting/QueuePriority.java
@@ -0,0 +1,24 @@
+package com.techcrack.DSA.Sorting;
+
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+public class QueuePriority {
+    static void priorityQueue(int[] arr) {
+        Queue<Integer> queue = new PriorityQueue<>();
+
+        for (int a : arr) {
+            queue.offer(a);
+            System.out.println(queue);
+            System.out.println(queue.peek());
+            int poll = queue.isEmpty() ? 0 : queue.poll();
+            System.out.println(poll);
+            queue.offer(poll);
+        }
+    }
+
+    public static void main(String[] args) {
+
+        priorityQueue(new int[]{3,2,3,1,2,4,5,5,6});
+    }
+}
