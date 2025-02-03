715824855a36862a3cdfe7d67c4bfd4b3a5df839
KaVin S <kavinadithya3@gmail.com>
2025-02-03 06:15:58 +0530

    Learning about subsets and subsequence

---
+++ src/main/java/com/techcrack/DSA/recursion/subsets/StringSubsets.java
@@ -0,0 +1,34 @@
+package com.techcrack.DSA.recursion.subsets;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class StringSubsets {
+    static void subsets(List<String> subsets, String processes, String unprocessed) {
+        if (unprocessed.isEmpty()) {
+            if (! processes.isEmpty())
+                subsets.add(processes);
+            System.out.println(processes);
+            return;
+        }
+
+        char ch = unprocessed.charAt(0);
+
+        unprocessed = unprocessed.substring(1);
+
+//        subsets(subsets, processes + " " + ch, unprocessed);
+        subsets(subsets, processes, unprocessed);
+        subsets(subsets, processes + " " + (int) ch, unprocessed);
+    }
+
+    public static void main(String[] args) {
+        List<String> subsets = new ArrayList<>();
+
+        String s = "abc";
+
+        subsets(subsets, "", s);
+
+        subsets.sort(String :: compareTo);
+//        System.out.println(subsets);
+    }
+}
