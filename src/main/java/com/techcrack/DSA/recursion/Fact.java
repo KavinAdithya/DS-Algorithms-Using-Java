62f91cbf3eea62d0218facfe57977bed1e12321f
KaVin S <kavinadithya3@gmail.com>
2025-02-05 04:49:28 +0530

    Solved some basic problems on recursion

--- src/main/java/com/techcrack/DSA/recursion/Fact.java
+++ src/main/java/com/techcrack/DSA/recursion/Fact.java
@@ -13,8 +13,32 @@ public class Fact {
         return n + sum(n - 1);
     }
 
+    static int fibonacciConstant(int a, int b, int n) {
+        if (n % 3 == 0) return a;
+        if (n % 3 == 1) return b;
+
+        return a ^ b;
+    }
+
+    static int fibo(int a, int b, int n)  {
+        if (n == 0) return a;
+        if (n == 1) return b;
+
+        return fibo(a, b, n - 1) ^ fibo(a, b, n - 2);
+    }
+
+    static int countLength(String str) {
+        if (str.isEmpty()) return 0;
+
+        return 1 + countLength(str.substring(1));
+    }
+
     public static void main(String[] args) {
-        System.out.println(fact(5));
-        System.out.println(sum(5));
+//        System.out.println(fact(5));
+//        System.out.println(sum(5));
+
+        System.out.println(fibonacciConstant(4, 3, 4));
+        System.out.println(fibo(4, 3, 4));
+        System.out.println(countLength("Java "));
     }
 }
--- src/main/java/com/techcrack/DSA/recursion/subsets/ArraySubsets.java
+++ src/main/java/com/techcrack/DSA/recursion/subsets/ArraySubsets.java
@@ -1,6 +1,7 @@
 package com.techcrack.DSA.recursion.subsets;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class ArraySubsets {
@@ -22,10 +23,37 @@ public class ArraySubsets {
         return outer;
     }
 
-    public static void main(String[] args) {
-        int[] arr = {1, 2, 3};
+    static List<List<Integer>> findSubsetsWithoutDuplicate(int[] arr) {
+        List<List<Integer>> outer = new ArrayList<>();
+        Arrays.sort(arr);
 
-        for (List<Integer> list : findSubsets(arr)) {
+        outer.add(new ArrayList<>());
+        int start = 0;
+        int end = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            start = 0;
+
+            if (i > 0 && arr[i - 1] == arr[i])
+                start = end;
+            end = outer.size();
+
+            for (int j = start; j < end; j++) {
+                List<Integer> inter = new ArrayList<>(outer.get(j));
+
+                inter.add(arr[i]);
+
+                outer.add(inter);
+            }
+        }
+
+        return outer;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 2};
+
+        for (List<Integer> list : findSubsetsWithoutDuplicate(arr)) {
             System.out.println(list);
         }
     }
