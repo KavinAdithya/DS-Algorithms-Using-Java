e3822a84b4cf91ce479952dc6e70518174810ae6
KaVin S <kavinadithya3@gmail.com>
2025-02-06 04:54:21 +0530

    Learn about permutation using recursion

--- src/main/java/com/techcrack/DSA/recursion/permutation/permutation.java
+++ src/main/java/com/techcrack/DSA/recursion/permutation/permutation.java
@@ -1,5 +1,10 @@
 package com.techcrack.DSA.recursion.permutation;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 public class permutation {
     static void stringPermutation(String processed, String unprocessed) {
         if (unprocessed.isEmpty()) {
@@ -16,10 +21,47 @@ public class permutation {
         }
     }
 
-    public static void main(String[] args) {
-        String name = "abc";
+    static int stringPermutationCount(String processed, String unprocessed) {
+        if (unprocessed.isEmpty()) return 1;
 
-        stringPermutation("", name);
+        char ch = unprocessed.charAt(0);
+        unprocessed = unprocessed.substring(1);
+        int count = 0;
+
+        for (int i = 0; i <= processed.length(); i++) {
+            String curProcessed = processed.substring(0, i) + ch + processed.substring(i);
+
+            count += stringPermutationCount(curProcessed, unprocessed);
+        }
+
+        return count;
+    }
+
+    static List<String> stringPermutationPersist(String processed, String unprocessed) {
+        if (unprocessed.isEmpty()) {
+            return Collections.singletonList(processed);
+        }
+
+        char curChar = unprocessed.charAt(0);
+        unprocessed = unprocessed.substring(1);
+        List<String> ans = new ArrayList<>();
+
+        for (int i = 0; i < processed.length() + 1; i++) {
+            String curProcess = processed.substring(0, i) + curChar + processed.substring(i);
+            ans.addAll(stringPermutationPersist(curProcess, unprocessed));
+        }
+
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        String name = "6abc";
+
+//        stringPermutation("", name);
 //        System.out.println(name.substring(2) + " " + 'c');
+
+//        System.out.println(stringPermutationPersist("", name));
+
+        System.out.println(stringPermutationCount("", name));
     }
 }
